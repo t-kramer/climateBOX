06388b8ef9d498c8dd18eebd215af232d2c60f01
Tobias Kramer <49641232+t-kramer@users.noreply.github.com>
2023-01-31 19:02:59 +1000

    Update program-climateBOX-v0.0.ino
    Updated contact information.

--- code/program-climateBOX-v0.0.ino
+++ code/program-climateBOX-v0.0.ino
@@ -14,26 +14,27 @@
   e) DS1302                   RTC module
 
 
-More information about the climateBOX and it's calibration can be found here:
+More information about the climateBOX can be found here:
 
 - Publication:
-
+Kramer, T., Garcia-Hansen, V., & Omrani, S. (2023). 
+climateBOX: a low-cost and open-source monitoring device for personal thermal comfort evaluation. 
+Energy and Buildings, 112830.
 
 - GitHub page:
+https://github.com/t-kramer/climateBOX
 
 
+The code is provided by:
+
+School of Architecture & Built Environment,
+Queensland University of Technology
+
+Contact:
+t.kramer@qut.edu.au
 
 
- The code was written by:
-  
- Tobias Kramer
- School of Architecture & Built Environment,
- Queensland University of Technology
-
- t.kramer@qut.edu.au
-
- 
- License:                       */
+License:                       */
 
 //______________________________________LIBRARIES______________________________________//
 
